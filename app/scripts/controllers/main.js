831c2305cdac719c26dfba32488ab01e55ba7311
Daniel McOrmond <daniel.mcormond@gmail.com>
2016-11-29 10:34:35 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reallyGoodEmailsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reallyGoodEmailsApp
+ */
+angular.module('reallyGoodEmailsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

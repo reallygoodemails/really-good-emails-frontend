5b8aeb87ee9197abd91c510c3ec9bf5a672ae7ed
Daniel McOrmond <daniel.mcormond@gmail.com>
2017-01-03 13:04:54 -0500

    Simplify search controller

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,23 +1,8 @@
 'use strict';
 
 angular.module('reallyGoodEmailsApp')
-  .controller('SearchCtrl', function($location, $scope, algolia, apiHost) {
+  .controller('SearchCtrl', function($scope) {
     $scope.search = {
-      query: $location.search().q || '',
-      hits: []
-    };
-    var client = algolia.Client('PBJZ5RMGND', '7181b52312010545c774c92fced72c69');
-    var index = client.initIndex('wp_searchable_posts');
-
-    $scope.$watch('search.query', function() {
-      index.search($scope.search.query)
-        .then(function searchSuccess(content) {
-          $scope.search.hits = content.hits;
-        }, function searchFailure(err) {
-      });
-    });
-
-    $scope.getLinkURL = function(hit) {
-      return hit.permalink.replace(apiHost, '');
+      query: ''
     };
   });

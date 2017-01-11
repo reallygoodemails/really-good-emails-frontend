d97967e7179db70f3fff362091d2c21658512f8a
Daniel McOrmond <daniel.mcormond@gmail.com>
2017-01-11 13:09:07 -0500

    Prepopulate categories in JSData store. Fixes #35

--- app/scripts/controllers/categories.js
+++ app/scripts/controllers/categories.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('reallyGoodEmailsApp')
-  .controller('CategoriesCtrl', function(DS) {
+  .controller('CategoriesCtrl', function(DS, categories) {
+
+    // Pre-populate some categories
+    DS.inject('categories', categories);
 
     // @see http://v2.wp-api.org/reference/categories/
     var params = {
@@ -10,7 +13,7 @@ angular.module('reallyGoodEmailsApp')
     };
 
     var vm = this;
-    vm.categories = [];
+    vm.categories = DS.getAll('categories');
 
     DS.findAll('categories', params)
       .then(function (categories) {
